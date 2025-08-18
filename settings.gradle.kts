a5c2ec04628b547528d6c42d598be60ce8bcf3b9
CapssMan <123011067+CapssMan@users.noreply.github.com>
2025-08-18 10:42:37 +0300

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,30 @@
 pluginManagement {
     repositories {
-        google()
+        google()  // Важно: должен быть первым
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android") {
+                useModule("com.android.tools.build:gradle:8.3.2")
+            }
+            if (requested.id.id == "org.jetbrains.kotlin.android") {
+                useVersion("1.9.20")
+            }
+        }
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 rootProject.name = "EconCalc"
 include(":app")
